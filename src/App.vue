99b0a9aadc14a80f63c6466def09819432439a8c
BiCute <bicute@naver.com>
2023-02-26 12:46:59 +0900

    프로젝트 헤더 영역 수정 / 미사용 컴포넌트 제거

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,30 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
-import Introduction from './components/Introduction.vue';
 </script>
 
 <template>
-    <header>
-        <img alt="site logo" class="logo" src="@/assets/logo.png" width="128" height="128" />
-
-        <div class="wrapper">
-            <Introduction msg="My Prompt" />
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
+    <header class="mb-4">
+        <div class="w-full flex justify-between">
+            <h1 class="text-3xl font-bold">My Prompt</h1>
+            <img alt="site logo" class="w-10 h-10" src="@/assets/logo.png" />
         </div>
+
+        <nav v-if="false">
+            <RouterLink to="/">Home</RouterLink>
+            <!-- <RouterLink to="/about">About</RouterLink> -->
+        </nav>
     </header>
     <router-view></router-view>
 </template>
 
 <style scoped>
-header {
-    line-height: 1.5;
-    max-height: 100vh;
-}
-
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
 
 nav {
     width: 100%;
     font-size: 12px;
     text-align: center;
-    margin-top: 2rem;
+    margin-top: 0.5rem;
+    margin-bottom: 2rem;
 }
 
 nav a.router-link-exact-active {
@@ -52,32 +43,5 @@ nav a {
 
 nav a:first-of-type {
     border: 0;
-}
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-
-    nav {
-        text-align: left;
-        margin-left: -1rem;
-        font-size: 1rem;
-
-        padding: 1rem 0;
-        margin-top: 1rem;
-    }
 }
 </style>
