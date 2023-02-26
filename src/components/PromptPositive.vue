19c46ebec00aa853fbf9b3faf8d44b3c6ae5a334
BiCute <bicute@naver.com>
2023-02-26 10:41:21 +0900

    클립보드 Copy시 Toast 작동

--- src/components/PromptPositive.vue
+++ src/components/PromptPositive.vue
@@ -11,17 +11,22 @@
                 <button @click="copyToClipboard">COPY</button>
             </div>
             <div class="h-24 border-t border-gray-200 p-4 overflow-scroll">
-                {{ filteredPrompt }}
+                {{ filteredPrompt ? filteredPrompt : '단어를 선택해 주세요'}}
             </div>
         </div>
     </div>
+    <Transition name="fade">
+        <Toast v-if="toastShow" :title="toastTitle" :description="toastDescription" />
+    </Transition>
 </template>
 
 <script>
 import { useImageDataStore } from '@/stores/imageData.js';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
+import Toast from '@/components/Toast.vue';
 
 export default {
+    components: { Toast },
     setup() {
         const imageDataStore = useImageDataStore();
 
@@ -31,14 +36,40 @@ export default {
                 .join(', ');
         });
 
+        // 토스트 시작
+        const toastShow = ref(false);
+        const toastTitle = ref('SUCCESS');
+        const toastDescription = ref('클립보드에 복사하였습니다.');
+        let timeoutId = null;
+
         const copyToClipboard = () => {
             navigator.clipboard.writeText(filteredPrompt.value);
+            triggerToast();
         };
+
+        const triggerToast = () => {
+            if (!filteredPrompt.value) {
+                toastTitle.value = 'FAILED';
+                toastDescription.value = '복사할 내용이 없습니다';
+            } else {
+                toastTitle.value = 'SUCCESS';
+                toastDescription.value = '클립보드에 복사하였습니다.';
+            }
+            toastShow.value = true;
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                toastShow.value = false;
+            }, 3000);
+        };
+        // 토스트 종료
 
         return {
             imageDataStore,
             filteredPrompt,
             copyToClipboard,
+            toastShow,
+            toastTitle,
+            toastDescription,
         };
     },
 };
